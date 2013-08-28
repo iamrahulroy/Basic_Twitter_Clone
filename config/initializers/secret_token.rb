6e5145b935d50accbdb6997b798566e71cfc1ac8
Rahul <techniciablog@gmail.com>
2013-08-28 06:30:21 +0530

    Initial Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicTwitterClone::Application.config.secret_token = '0594ec692e8089d6b85b8329c5ecd296c3895f41d239c8743748c978c785fe44ef96c0954cf7036341f8497190fa7864f1ada6139f87e57c5dd07d00a59b7a8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BasicTwitterClone::Application.config.session_store :cookie_store, key: '_Basic_Twitter_Clone_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BasicTwitterClone::Application.config.session_store :active_record_store
